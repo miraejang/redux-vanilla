a8a1fcf923501a4f91c37680aa9c4514bf78154d
miraejang <mirae611@nate.com>
2022-07-21 16:42:20 +0900

    impove
    if-else -> switch and add/minus string -> variable

--- src/index.js
+++ src/index.js
@@ -6,25 +6,27 @@ const number = document.getElementById('number');
 
 number.innerText = 0;
 
+const ADD = 'add';
+const MINUS = 'minus';
+
 const countModifier = (count = 0, action) => {
-  if (action.type === 'add') {
-    return count + 1;
-  } else if (action.type === 'minus') {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 const countStore = createStore(countModifier);
-
-countStore.dispatch({ type: 'add' });
 
 const updateText = () => {
   number.innerText = countStore.getState();
 };
 countStore.subscribe(updateText);
-const handleAdd = () => countStore.dispatch({ type: 'add' });
-const handleMinus = () => countStore.dispatch({ type: 'minus' });
+const handleAdd = () => countStore.dispatch({ type: ADD });
+const handleMinus = () => countStore.dispatch({ type: MINUS });
 
 add.addEventListener('click', handleAdd);
 minus.addEventListener('click', handleMinus);
