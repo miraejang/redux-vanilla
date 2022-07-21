06736515afd9857027670fd46506b3f16d2f16b6
miraejang <mirae611@nate.com>
2022-07-21 16:31:42 +0900

    redux로 변경

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.getElementById('number');
 
-let count = 0;
-number.innerText = count;
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
+  if (action.type === 'add') {
+    return count + 1;
+  } else if (action.type === 'minus') {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: 'add' });
 
 const updateText = () => {
-  number.innerText = count;
+  number.innerText = countStore.getState();
 };
-const handleAdd = () => {
-  count++;
-  updateText();
-};
-const handleMinus = () => {
-  const lowest = count - 1 < 0;
-  count = lowest ? 0 : count - 1;
-  updateText();
-};
+countStore.subscribe(updateText);
+const handleAdd = () => countStore.dispatch({ type: 'add' });
+const handleMinus = () => countStore.dispatch({ type: 'minus' });
 
 add.addEventListener('click', handleAdd);
 minus.addEventListener('click', handleMinus);
