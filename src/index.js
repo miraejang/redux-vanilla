00dc4354008e4d5353b2f8185ddc93a21d37a988
miraejang <mirae611@nate.com>
2022-07-25 16:49:12 +0900

    vanilla js to do list

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
-import { createStore } from 'redux';
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.getElementById('number');
-
-number.innerText = 0;
-
-const ADD = 'add';
-const MINUS = 'minus';
-
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
+const createToDo = toDo => {
+  console.log(toDo);
+  const li = document.createElement('li');
+  li.innerText = toDo;
+  ul.appendChild(li);
 };
-const countStore = createStore(countModifier);
 
-const updateText = () => {
-  number.innerText = countStore.getState();
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  createToDo(toDo);
+  input.value = '';
 };
-countStore.subscribe(updateText);
-const handleAdd = () => countStore.dispatch({ type: ADD });
-const handleMinus = () => countStore.dispatch({ type: MINUS });
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+form.addEventListener('submit', onSubmit);
